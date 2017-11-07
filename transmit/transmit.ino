1c7e78e6b91285b4636f1882ef4085b2f537af11
Winston <winstona@ad.msoe.edu>
2017-11-06 20:50:37 -0600

    Makes a square wave when transmitting.

--- transmit/transmit.ino
+++ transmit/transmit.ino
@@ -1,10 +1,12 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <inttypes.h>
 
 RF24 radio(7, 8);
 
 const byte rxAddr[6] = "00001";
+bool sample = LOW;
 
 void setup()
 {
@@ -17,8 +19,7 @@ void setup()
 
 void loop()
 {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  
-  delay(1000);
+  sample = !sample;
+  radio.write(&sample, sizeof(sample));
+  delay(1);
 }
