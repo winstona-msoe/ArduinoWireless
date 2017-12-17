7f6e32fd98328bacf9ff1f2697ed6b4a4ecd03e3
ashwin2211 <ashwinkudmulwar@ymail.com>
2017-12-16 21:04:49 -0600

    New audio receiving code

--- receive/receive.ino
+++ receive/receive.ino
@@ -1,16 +1,15 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
 RF24 radio(7, 8);
 const byte rxAddr[6] = "00001";
-
 void setup()
 {
   while (!Serial);
   Serial.begin(9600);
-  radio.setChannel(1);
+  pinMode(9, OUTPUT);
   radio.begin();
+  radio.setChannel(1);
   radio.openReadingPipe(0, rxAddr);
   radio.startListening();
 }
@@ -19,9 +18,11 @@ void loop()
 {
   if (radio.available())
   {
-    char text[32] = {0};
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    bool sample;
+    radio.read(&sample, sizeof(sample));
+    digitalWrite(9, sample);
+   // Serial.println(text);
   }
-}
+}  
+
 
